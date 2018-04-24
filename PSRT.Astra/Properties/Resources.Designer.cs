2426b7a16fb2db29a29ba61fdc2fbff14b40db33
George Bott <insertlogic@gmail.com>
2018-04-25 00:06:32 +0100

    The last bits of text that slipped past

--- PSRT.Astra/Properties/Resources.Designer.cs
+++ PSRT.Astra/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PSO2Patcher.Properties {
+namespace PSRT.Astra.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PSO2Patcher.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSO2Patcher.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSRT.Astra.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
