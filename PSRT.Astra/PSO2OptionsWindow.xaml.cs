d5b0cd0b99560e98ad5163926051e4d238a7d2e8
AJ Martinez <aj@tyronesama.moe>
2018-05-21 17:24:08 -0700

    Add strings and UI for PSO2 graphics options

---
+++ PSRT.Astra/PSO2OptionsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PSRT.Astra
+{
+    /// <summary>
+    /// Interaction logic for PSO2OptionsWindow.xaml
+    /// </summary>
+    public partial class PSO2OptionsWindow : Window
+    {
+        public PSO2OptionsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
