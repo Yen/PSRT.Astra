73c2db23c8d347d14234c40bb316c722196c7558
George Bott <george@bott.gg>
2020-06-10 12:35:38 +0100

    Changed user-agent to use that of the tweaker temporarily

--- PSRT.Astra/Models/ArksLayer/ArksLayerHttpClient.cs
+++ PSRT.Astra/Models/ArksLayer/ArksLayerHttpClient.cs
@@ -11,7 +11,8 @@ namespace PSRT.Astra.Models.ArksLayer
     {
         public ArksLayerHttpClient()
         {
-            DefaultRequestHeaders.Add("User-Agent", "PSRT.Astra");
+            //DefaultRequestHeaders.Add("User-Agent", "PSRT.Astra");
+            DefaultRequestHeaders.Add("User-Agent", "PSO2.Tweaker.v6.0.1.4");
             DefaultRequestHeaders.Add("Cache-Control", "no-cache, no-store, must-revalidate");
         }
     }
