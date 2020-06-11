801ecd168e7ecaa7246c92d347650f651f1bce8b
George Bott <george@bott.gg>
2020-06-11 14:55:20 +0100

    Added a "fix" to ArksLayerHttpClient that fetches the current version of the tweaker from the arks-layer website to use as the user-agent as everything else is now blocked, even "PSRT.Astra"

--- PSRT.Astra/Models/ArksLayer/ArksLayerHttpClient.cs
+++ PSRT.Astra/Models/ArksLayer/ArksLayerHttpClient.cs
@@ -4,15 +4,47 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace PSRT.Astra.Models.ArksLayer
 {
     public class ArksLayerHttpClient : HttpClient
     {
+        private static object _TweakerVersionLock = new object();
+        private static Version _TweakerVersion;
+
         public ArksLayerHttpClient()
         {
+            // this is very dumb but the arks-layer servers have been changed to only accept downloads from
+            // clients with a user-agent representing the current version of the tweaker and nothing else.
+            // the original agreed upon PSRT.Astra user-agent has been blocked
+            if (_TweakerVersion == null)
+            {
+                lock (_TweakerVersionLock)
+                {
+                    // weird concurrency double check dont think too much about it
+                    if (_TweakerVersion == null)
+                    {
+                        App.Logger.Info(nameof(ArksLayerHttpClient), "Downloading PSO2 Tweaker version info from arks-layer feed");
+                        using (var client = new HttpClient())
+                        {
+                            // run the async task in the thread pool as as to not cause a deadlock because constructors must be synchronous
+                            var tweakerVersionXml = Task.Run(async () => await client.GetStringAsync("https://arks-layer.com/justice/version2.xml")).GetAwaiter().GetResult();
+
+                            App.Logger.Info(nameof(ArksLayerHttpClient), "Parsing PSO2 Tweaker version info from xml");
+                            var document = new XmlDocument();
+                            document.LoadXml(tweakerVersionXml);
+
+                            var versionString = document["item"]["version"].InnerText;
+                            _TweakerVersion = Version.Parse(versionString);
+                            App.Logger.Info(nameof(ArksLayerHttpClient), $"PSO2 Tweaker version found to be {_TweakerVersion}");
+                        }
+                    }
+                }
+            }
+            DefaultRequestHeaders.Add("User-Agent", $"PSO2.Tweaker.v{_TweakerVersion}");
+
             //DefaultRequestHeaders.Add("User-Agent", "PSRT.Astra");
-            DefaultRequestHeaders.Add("User-Agent", "PSO2.Tweaker.v6.0.1.4");
             DefaultRequestHeaders.Add("Cache-Control", "no-cache, no-store, must-revalidate");
         }
     }
